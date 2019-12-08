e4b9558f1c3a8ad12a679c3be5eaa782bb17f2ee
Loovax <bryan@loovax.com>
2019-12-08 11:40:35 -0500

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { MyApp } from '../app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { ShopPage } from '../pages/shop/shop';
+import { ShopPage } from '../pages/shop/shop/shop';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 

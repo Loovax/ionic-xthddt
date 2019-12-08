135c0c07cce9beb12dff17803664443d14b9d15d
Loovax <bryan@loovax.com>
2019-12-08 11:18:18 -0500

    3 files modified, 1 deleted

--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Shop } from '../shop/shop';
 
+import { ShopPage } from '../shop/shop';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
